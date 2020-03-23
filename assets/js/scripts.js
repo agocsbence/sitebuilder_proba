4edd76044a858a22640294fb2cda1950612406b9
Bence Agocs <bence@harom.studio>
2020-03-23 03:19:41 +0100

    mobile menu with animation added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,23 @@
-let menuBtn = document.getElementById("menu-button");
+let menuBtn = document.getElementById("menu-button"),
+    closeBtn = document.getElementById("closeBtn"),
+    mobileMenuBtn = document.getElementById("mobile-menu-button")
+    mobileMenu = document.getElementById("mobile-menu"),
+    mobileMenuHeader = document.getElementById("mobile-menu-header");
 
 menuBtn.addEventListener("click", function() {
     console.log("#menu-button clicked!");
+    mobileMenu.classList.toggle("opened");
+    mobileMenuHeader.classList.toggle("opened");
+});
+
+closeBtn.addEventListener("click", function() {
+    console.log("#closeBtn clicked!");
+    mobileMenu.classList.toggle("opened");
+    mobileMenuHeader.classList.toggle("opened");
+});
+
+mobileMenuBtn.addEventListener("click", function() {
+    console.log("#menu-button clicked!");
+    mobileMenu.classList.toggle("opened");
+    mobileMenuHeader.classList.toggle("opened");
 });

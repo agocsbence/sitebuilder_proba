778aab5c95fb5cce7c1b16e321cd22b70a095564
Bence Agocs <bence@harom.studio>
2020-03-23 13:43:46 +0100

    mobile menu language selector toggle added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,9 @@ let menuBtn = document.getElementById("menu-button"),
     closeBtn = document.getElementById("closeBtn"),
     mobileMenuBtn = document.getElementById("mobile-menu-button")
     mobileMenu = document.getElementById("mobile-menu"),
-    mobileMenuHeader = document.getElementById("mobile-menu-header");
+    mobileMenuHeader = document.getElementById("mobile-menu-header"),
+    langSelect = document.getElementById("languageSelector"),
+    body = document.getElementsByTagName("body")[0];
 
 menuBtn.addEventListener("click", function() {
     console.log("#menu-button clicked!");
@@ -20,4 +22,13 @@ mobileMenuBtn.addEventListener("click", function() {
     console.log("#menu-button clicked!");
     mobileMenu.classList.toggle("opened");
     mobileMenuHeader.classList.toggle("opened");
+});
+
+langSelect.addEventListener("click", function() {
+    body.classList.toggle("lang-hu");
+    if( body.classList.contains("lang-hu")) {
+        langSelect.innerHTML = "Magyar";
+    } else {
+        langSelect.innerHTML = "English";
+    }
 });
